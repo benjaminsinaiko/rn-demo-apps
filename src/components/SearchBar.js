60d9b86d44a2d9b3932126050af4582515c97abe
benjaminsinaiko <ben.jamin@outlook.com>
2020-02-20 13:06:25 -0600

    Add basic search screen with search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+const SearchBar = () => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name='search' style={styles.iconStyle} />
+      <TextInput style={styles.inputStyle} placeholder='Search' />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 10,
+    backgroundColor: '#f0eeee',
+    height: 50,
+    marginHorizontal: 15,
+    borderRadius: 5,
+    flexDirection: 'row'
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15
+  }
+});
+
+export default SearchBar;
